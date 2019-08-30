9a979bf81f12b9ce7bf814b888eb27cfcbee1837
sam-liaw <sam.liaw@creativearktech.com>
2019-08-30 13:03:07 +0800

    1. git 排除特定檔案 2. 實做寫入 MSTest 測試專案 Output 裡的 Log 功能

---
+++ MSTestLog/MSTestLog/MSTestLog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace MSTestLog
+{
+    public class MSTestLog
+    {
+        private static StringBuilder _testOutputBuilder;
+        private static StringWriter _testOutputWriter;
+        private static TextWriter _originalWriter;
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public static void Initialize()
+        {
+            _testOutputBuilder = new StringBuilder();
+            _testOutputWriter = new StringWriter(_testOutputBuilder);
+            _originalWriter = Console.Out;
+            Console.SetOut(_testOutputWriter);
+        }
+
+        public static string WriteLine(string msg)
+        {
+            string ret_ = writeLine(_originalWriter, msg);
+
+            Console.SetOut(_originalWriter);
+            _testOutputWriter.Dispose();
+
+            return ret_;
+        }
+
+        private static string writeLine(TextWriter textWriter, string errorMessage)
+        {
+            textWriter.WriteLine(errorMessage);
+            return errorMessage;
+        }
+    }
+}
